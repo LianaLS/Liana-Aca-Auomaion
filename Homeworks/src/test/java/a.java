34157bb99eb7ea3a2f4108b665a92fe32b127b35
LianaLS <54749019+LianaLS@users.noreply.github.com>
2022-06-18 19:30:09 +0400

    fff

---
+++ Homeworks/src/test/java/a.java
@@ -0,0 +1,19 @@
+//import org.openqa.selenium.WebDriver;
+//import org.openqa.selenium.chrome.ChromeDriver;
+
+import static java.lang.System.*;
+
+public class a {
+    public static void main(String[] args) {
+        String  a="hello";
+        String b="welcggome";
+        String c=a;
+         a= b;
+         b=c;
+        System.out.println(a);
+        System.out.println(b);
+        //WebDriver dr = new ChromeDriver();
+    }
+}
+// % x%y veradarcnum e mnacordy 7/2 kveradarcni 1
+
